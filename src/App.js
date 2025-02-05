6f6700cef9e3b6e35dc85b286128b12d4a0bdd98
wliuye <161380356+wliuye@users.noreply.github.com>
2025-02-04 18:47:50 -0800

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Wimym Andrea Liu Ye and is
+        {" "}
+        <a href="https://github.com/wliuye/wimyms-weather-app" 
+        target="_blank"
+        >
+           open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
