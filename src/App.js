48506421492c4cd7a471e707fc1262af962b3957
wliuye <161380356+wliuye@users.noreply.github.com>
2025-02-05 18:38:18 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Wimym Andrea Liu Ye and is{" "}
-        <a
-          href="https://github.com/wliuye/wimym-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Wimym Andrea Liu Ye and is{" "}
+          <a
+            href="https://github.com/wliuye/wimym-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
