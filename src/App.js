4cafab6ad35e416c1f88364591c2cc8e1b17f11f
wliuye <161380356+wliuye@users.noreply.github.com>
2025-02-05 18:23:26 -0800

    fixed structure

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Wimym Andrea Liu Ye and is
-        {" "}
-        <a href="https://github.com/wliuye/wimyms-weather-app" 
-        target="_blank"
+        This project was coded by Wimym Andrea Liu Ye and is{" "}
+        <a
+          href="https://github.com/wliuye/wimym-react-app"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-           open-sourced on Github
+          open-sourced on Github
         </a>
       </footer>
     </div>
