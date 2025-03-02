f88257bf13cbff2a038c44bbb5b1ec6cb0404092
wliuye <161380356+wliuye@users.noreply.github.com>
2025-03-01 17:15:16 -0800

    Fixed WeatherIcon.js added correct code mapping

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,7 @@ import "./Weather.css";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
 
+
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
